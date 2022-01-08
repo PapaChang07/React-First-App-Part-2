710bdc06d37866064bc3e6dfcc10625a1a16bb9c
Norman Chang <sangheilios081@gmail.com>
2022-01-08 13:51:14 -0800

    Rough Draft

--- src/MyApp.js
+++ src/MyApp.js
@@ -34,9 +34,14 @@ function MyApp() {
     return (
         <div className="container">
             <Table characterData={characters} removeCharacter={removeOneCharacter} />
-            <Form />
+            <Form handleSubmit = {updateList}/>
         </div>
     )
+    function updateList(person) {
+        setCharacters([...characters, person]);
+    }
+
+    
 }   
 
 export default MyApp;
