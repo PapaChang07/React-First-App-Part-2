c2c479b531b6831a666eae8882aad5f62f3e8993
Norman Chang <sangheilios081@gmail.com>
2022-01-18 11:22:47 -0800

    Nice

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,28 +1,19 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import Table from './Table'
 import Form from './Form';
 
-// const characters = [
-//     {
-//         name: 'Charlie', 
-//         job: 'Janitor',
-//     },
-//     {
-//         name: 'Mac', 
-//         job: 'Bouncer',
-//     },
-//     {
-//         name: 'Dee', 
-//         job: 'Aspiring Actress',
-//     },
-//     {
-//         name: 'Dennis', 
-//         job: 'Bartender',
-//     },
-// ];
 
 function MyApp() { 
     const [characters, setCharacters] = useState([]); 
+    
+    useEffect(() => {
+        fetchAll().then( result => {
+           if (result)
+              setCharacters(result);
+        });
+    }, [] );
+     
     
     function removeOneCharacter (index) {
         const updated = characters.filter((character, i) => {
@@ -31,15 +22,43 @@ function MyApp() {
         setCharacters(updated);
     }
 
+    function updateList(person) { 
+        makePostCall(person).then( result => {
+        if (result && result.status === 200)
+           setCharacters([...characters, person] );
+        });
+    }
+
+    async function fetchAll(){
+        try {
+           const response = await axios.get('http://localhost:5000/users');
+           return response.data.users_list;     
+        }
+        catch (error){
+           //We're not handling errors. Just logging into the console.
+           console.log(error); 
+           return false;         
+        }
+    }
+
+    async function makePostCall(person){
+        try {
+           const response = await axios.post('http://localhost:5000/users', person);
+           return response;
+        }
+        catch (error) {
+           console.log(error);
+           return false;
+        }
+    }
+
     return (
         <div className="container">
             <Table characterData={characters} removeCharacter={removeOneCharacter} />
             <Form handleSubmit = {updateList}/>
         </div>
     )
-    function updateList(person) {
-        setCharacters([...characters, person]);
-    }
+
 
     
 }   
